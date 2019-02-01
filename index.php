989dbb17bfb26f9c04c059e01e245c362aad6f16
Dr. Zeb <drzeb@machomath.com>
2019-02-01 13:07:06 -0600

    Initial Commit

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
   defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
   header("Location: public" . DS . "frontend" . DS . "home.php");
+
+  
+  //added a new line
 ?>
---
+++ try.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dr. Zeb
+ * Date: 2/1/2019
+ * Time: 1:04 PM
+ */
+?>

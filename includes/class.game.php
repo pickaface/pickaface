d5f3f8cfafeab16738e06d4364778ecc31b18b90
Notiyal <shwetanotiyal@gmail.com>
2019-03-01 11:30:19 -0600

    New class.game.php added

---
+++ includes/class.game.php
@@ -0,0 +1,47 @@
+<?php
+
+// Profile -
+// Id
+// Name
+// Logo
+// Photo
+// Choose Player (options among different pick and nick)
+//
+// Settings -
+// Language
+// Sound/Mute
+// Game level (Easy/Medium/Hard)
+//
+//
+// About -
+// Catogory
+// Instruction
+// Description
+// Rating
+//
+// Profile -
+// Number of players(Single, Multiplayer(Local, Online, Group), Play with Computer)
+// Buy a points to achieve level (Bidding)
+// Register (to Keep your score)
+// guest player
+// No. of plays (today)
+// Max point scored by you
+// Highest score
+//
+//
+// Other attributer-
+// Related games
+// Tags (Eg #war, #fight, #Action #Shooting)
+// Share
+// Option (mobile/Desktop/Both)
+// Download game
+// Restriction(Age info)
+// Author
+//
+//
+// Screen size
+// Did you like this Game?
+// Review/Feed back
+
+
+ ?>
